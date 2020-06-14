9ba7a77c004eca784ebdee3848893f3983e416a1
Martin Burch <MartinB@mmc.school.nz>
2020-06-14 19:14:53 +1200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace image_sources
+{
+    public partial class Form1 : Form
+    {
+        Image alien = Properties.Resources.alien1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
